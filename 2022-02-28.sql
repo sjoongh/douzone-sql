0c8d3e486d76ca3394f2f30abac4900c13151bb1
sjoongh <az45687@naver.com>
2022-02-28 12:32:58 +0900

    프로시저

---
+++ 2022-02-28.sql
@@ -0,0 +1,211 @@
+-- PL/SQL
+/*
+create [or replace] procedure 프로시저명 ( -- or replace는 생략해도 무관, 프로시저가 존재하면 변경됨
+    변수이름 in 데이터타입, 변수이름 in 데이터타입, ...
+)
+is
+
+Begin
+    실행(필수)
+End;
+
+---------------------------
+-- 실행 형식>
+execute 프로시저명
+exec 프로시저명(값, 값...)
+*/
+
+declare
+    vno emp2.empno%type;
+    vname varchar2(20);
+begin
+    select empno, name into vno, vname
+        from emp2
+        where empno = &empno; -- $empno을 통해 값 입력받기
+        dbms_output.put_line('입력된 사번 : ' || vno || ' <---> ' || vname);
+end;
+
+----------------------------
+select * from emp2;
+
+create or replace PROCEDURE p_test ( 
+    name in varchar2, -- in은 값을 받아옴, out은 값을 내보냄
+    su in number
+)
+is
+begin
+    dbms.output.put_line(name || '님 점수는 : ' || su);
+end;
+
+exec p_test('김연아', 99);
+------------------------------
+select * from userlist;
+drop table userlist;
+
+create table userlist (
+    id varchar2(10),
+    name varchar2(20),
+    age number,
+    addr varchar(50)
+);
+
+create or replace procedure p_userlist (
+    id in userlist.id%type := 'kingsmile',
+    name in userlist.name%type DEFAULT '도연이',
+    age in userlist.age%type := 10,
+    addr in userlist.addr%type := null
+)
+is 
+begin
+    insert into userlist VALUES(id, name, age, addr);
+    dbms_output.put_line('insert정보는 : ' || id||name||age||addr);
+end;
+
+
+----------------------------------
+-- 프로시저 실행시 주석을 없애야함
+-- 전체필드 추가
+exec p_userlist('yuna','김연아',20,'군포');
+exec p_userlist; -- default value input
+exec p_userlist(id=>'박보검', age=>33); -- 원하는 필드값만 추가 가능
+
+select * from userlist;
+------------------------------------------
+declare
+    su number := 90;
+begin
+    DBMS_OUTPUT.PUT_LINE('Program started. ');
+    IF ( su >= 90 ) then
+        DBMS_OUTPUT.PUT_LINE('A');
+    ELSIF ( su >= 80 ) then
+        DBMS_OUTPUT.PUT_LINE(su || 'B');
+    end if;
+    DBMS_OUTPUT.PUT_LINE('Program end. ');
+end;
+------------------------------
+declare
+    su number := 90;
+    grade varchar2(2);
+begin
+    IF ( su >= 90 ) then grade := 'A';
+    ELSIF ( su >= 80 ) then grade := 'B';
+    ELSIF ( su >= 80 ) then grade := 'C';
+    ELSIF ( su >= 80 ) then grade := 'D';
+    else grade := 'F';
+    end if;
+    DBMS_OUTPUT.PUT_LINE('당신의 : ' || su || '점 ==> ' || grade || '학점입니다.');
+end;
+------------------------------
+/*
+for 변수이름 in 시작..끝 loop
+    반복실행;
+end loop */
+
+begin
+    for i in 1..100 loop
+        if mod(i, 2) = 0 then DBMS_OUTPUT.PUT_LINE(i || ' ==> even!!');
+        else DBMS_OUTPUT.PUT_LINE(i || ' ==> odd');
+        end if;
+    end loop;
+end;
+-------------------------------
+begin
+    for su_list in (
+        select 1 as num from dual
+        union all
+        select 2 as num from dual
+        union all
+        select 3 as num from dual
+        union all
+        select 4 as num from dual
+    )
+    loop
+        if mod(su_list.num, 2) = 0 then DBMS_OUTPUT.PUT_LINE(su_list.num || ' ==> even!!');
+        else DBMS_OUTPUT.PUT_LINE(su_list.num || ' ==> odd!!');
+        end if;
+    end loop;
+end;
+
+------------------------
+/*
+WHILE 조건식 LOOP
+    실행문장;
+    증감식;
+END LOOP;
+*/
+
+declare
+    su number := 0;
+begin
+    while su < 50 loop
+        su := su + 1; -- 증감식
+        DBMS_OUTPUT.PUTLINE('현재 값은' || su);
+    end loop;
+end;
+
+------------------------------
+-- 1~10 까지 출력
+begin
+    for i in 1..10 loop
+        DBMS_OUTPUT.PUT_LINE(i);
+    end loop;
+end;
+-- 1~10 사이의 짝수만 출력
+begin
+    for i in 1..10 loop
+        if mod(i, 2) = 0 then DBMS_OUTPUT.PUT_LINE(i || ' 짝수입니다.');
+        end if;
+    end loop;
+end;
+-- 2단의 구구단 출력
+begin
+    for i in 1..9 loop
+            DBMS_OUTPUT.PUT_LINE(2 || ' * ' || i || ' = ' || 2*i );
+    end loop;
+end;
+
+-- 사용자 입력(단수)를 받아 구구단 출력 DECLARE vdan NUMBER(1) := &dan &은 사용자 입력
+DECLARE
+    answer number(1) := &dan;
+begin
+    for i in 1..9 loop
+        DBMS_OUTPUT.PUT_LINE(answer || ' * ' || i || ' = ' || answer*i );
+    end loop;
+end;
+    
+
+-- 구구단을 2중 반복문
+begin
+    for i in 2..9 loop
+        for j in 1..9 loop
+            DBMS_OUTPUT.PUT_LINE(i || ' * ' || j || ' = ' || j*i );
+        end loop;
+    end loop;
+end;
+-- 1~10까지 총 합계 구하기(loop)
+declare
+    result number(10) := 0;
+begin
+    for i in 1..10 loop
+        result := result + i;
+    end loop;
+        DBMS_OUTPUT.PUT_LINE(result);
+end;
+--------- 프로시저 찾기 ----------------------------------
+SELECT * FROM user_objects
+    WHERE LOWER(object_type)='procedure'; -- 해당 계정이 갖고있는 프로시저 리스트 보기
+
+SELECT text FROM user_source
+    WHERE LOWER(NAME) = 'p_test'; -- p_test 프로시저의 만들어진 내용(text)보기
+-----------------------------------------------
+declare
+    v_emprow emp%rowtype;
+begin
+    for i in 1..5 loop
+        select  into v_emprow 
+        from ( select * from ( select * from emp order by sal desc ) 
+        where rownum <= i )  where rownum <= 5;
+    end loop;
+end;
+-----------------------------------------------
+select * from emp order bt sal desc;
